1d04d79f032d329d142f0730a02d3b29121fd6dc
judemiller50 <judemiller50@gmail.com>
2026-01-05 07:22:41 +0000

    Jan 5, 2026, 1:22 AM

---
+++ src/intakefunctions.cpp
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "pros/adi.hpp"
+#include "pros/misc.h"
+#include "pros/motors.hpp"
+//needed definitions
+pros::Motor intakelow(7, pros::MotorGearset::green);
+pros::Motor intakehigh(8, pros::MotorGearset::green);
+//pneumatics
+pros::adi::Pneumatics matchload=pros::adi::Pneumatics('a',true);
+pros::adi::Pneumatics scorer=pros::adi::Pneumatics('b',true);
+
+void intakein() {
+    intakelow.move_velocity(100);
+    intakehigh.move_velocity(100);
+    scorer.extend();
+}
+void outup() {
+    intakelow.move_velocity(100);
+    intakehigh.move_velocity(100);
+    scorer.extend();
+}
+void outdown() {
+    intakelow.move_velocity(-100);
+    intakehigh.move_velocity(-100);
+}
+void outmiddle() {
+    intakelow.move_velocity(100);
+    intakehigh.move_velocity(-100);
+    scorer.retract();
+}
