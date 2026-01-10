dd7c23b762ca960829eb96cedf96d9fa1123b612
judemiller50 <judemiller50@gmail.com>
2026-01-10 21:59:46 +0000

    Jan 10, 2026, 3:59 PM

--- src/pidtest.cpp
+++ src/pidtest.cpp
@@ -11,10 +11,8 @@ void pidtest(lemlib::Drivetrain drivetrain,lemlib::OdomSensors sensors,lemlib::E
     pros::delay(50);
     master.print(0, 0, test.c_str());
     //add stuff later
-    double p=0;
-    double i=0;
-    double d=0;
-    double g=0;
+    double val = [0,0,0,0];
+    int s=0;
     //defaults
     if (type=="drive") {
         p=20;
@@ -22,6 +20,15 @@ void pidtest(lemlib::Drivetrain drivetrain,lemlib::OdomSensors sensors,lemlib::E
         d=1;
         g=12;
         lemlib::ControllerSettings angular_controller(10,0,0.5,0,0,0,0,0,0);
+        bool complete=false;
+        while (!complete) {
+            std::string valstr="p:"+std::to_string(p)+" i:"+std::to_string(i)+" d:"+std::to_string(d)+" g:"+std::to_string(g);
+            master.clear_line(1);
+            pros::delay(50);
+            master.print(1, 0,valstr.c_str());
+            master
+            pros::delay(20);
+        }
     }
     else if (type=="turn") {
         p=10;
