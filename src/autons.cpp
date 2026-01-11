56ebf8f7555fdee7e89b1b2f74208ce1cbed9916
judemiller50 <judemiller50@gmail.com>
2026-01-11 23:15:14 +0000

    Jan 11, 2026, 5:15 PM

--- src/autons.cpp
+++ src/autons.cpp
@@ -9,7 +9,7 @@
 void autonlb(lemlib::Chassis chassis) {
     //left blue
     chassis.setPose(0,0,0);
-    chassis.moveToPose(0, 10, 0, 1000);
+    
 }
 void autonrb(lemlib::Chassis chassis) {
     //right blue
@@ -34,22 +34,33 @@ ASSET(skillsp4_txt);
 void autonskills(lemlib::Chassis chassis) {
     //skills
     pros::adi::Pneumatics matchload=pros::adi::Pneumatics('a',true);
+    //setup
     chassis.setPose(0,0,0);
+    //prep for load
     matchload.extend();
+    //drive to match loader
     chassis.follow(skillsp1_txt, 10, 10000);
+    //intake in blobks for 5 sec
     intakein();
     pros::delay(5000);
     intakestop();
+    //stop, then drive to opposite side of big goal
     chassis.follow(skillsp2_txt, 10, 10000);
+    //score blocks in big goal for 5 sec
     outup();
     pros::delay(5000);
     intakestop();
+    //stop, then back up to opposite match loader
     chassis.follow(skillsp3_txt, 10, 10000);
+    //intake in blocks for 5 sec
     intakein();
     pros::delay(5000);
     intakestop();
+    //stop, then drive to big goal again
     chassis.follow(skillsp4_txt, 10, 10000);
     outup();
     pros::delay(5000);
     intakestop();
+    //stop, retract match load  to prep for driver control
+    matchload.retract();
 }
