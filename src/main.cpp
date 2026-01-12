cb99559597c570c27a95e35ca23a3edb2d390b0a
judemiller50 <judemiller50@gmail.com>
2026-01-12 16:22:39 +0000

    Jan 12, 2026, 10:22 AM

--- src/main.cpp
+++ src/main.cpp
@@ -106,12 +106,6 @@ lemlib::Chassis chassis(drivetrain, // drivetrain settings
 
 );
 lemlib::Chassis* chassisptr=&chassis;
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
