7b2a3fb6834932f7c3afb756741e71cee367548b
judemiller50 <judemiller50@gmail.com>
2026-01-22 23:10:53 +0000

    Jan 22, 2026, 5:10 PM

--- src/main.cpp
+++ src/main.cpp
@@ -76,8 +76,8 @@ lemlib::ControllerSettings lateral_controller(0, // proportional gain (kP)
 lemlib::ControllerSettings angular_controller(-9.8, // proportional gain (kP)
                                               0, // integral gain (kI)
                                               -67, // derivative gain (kD)
-                                              1, // anti windup
-                                              0, // small error range, in degrees
+                                              2, // anti windup
+                                              1, // small error range, in degrees
                                               8, // small error range timeout, in milliseconds
                                               0, // large error range, in degrees
                                               0, // large error range timeout, in milliseconds
