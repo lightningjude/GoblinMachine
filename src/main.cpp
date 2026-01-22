982ad69fd5798bedf62160b3e6c51978beb2f06f
judemiller50 <judemiller50@gmail.com>
2026-01-22 22:51:08 +0000

    Jan 22, 2026, 4:51 PM

--- src/main.cpp
+++ src/main.cpp
@@ -73,7 +73,7 @@ lemlib::ControllerSettings lateral_controller(0, // proportional gain (kP)
 );
 
 // angular PID controller
-lemlib::ControllerSettings angular_controller(0.2, // proportional gain (kP)
+lemlib::ControllerSettings angular_controller(-10, // proportional gain (kP)
                                               0, // integral gain (kI)
                                               0, // derivative gain (kD)
                                               0, // anti windup
