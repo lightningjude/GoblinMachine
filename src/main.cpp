9c4bba8429dd5a012cff1d618deff5cdfc549749
judemiller50 <judemiller50@gmail.com>
2026-01-23 20:06:27 +0000

    Jan 23, 2026, 2:06 PM

--- src/main.cpp
+++ src/main.cpp
@@ -62,9 +62,9 @@ lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to nullpt
 
 //PID setup
 // lateral PID controller
-lemlib::ControllerSettings lateral_controller( 20, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller( 60, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              80, // derivative gain (kD)
+                                              110, // derivative gain (kD)
                                               0, // anti windup: 3
                                               0, // small error range, in inches: 1
                                               0, // small error range timeout, in milliseconds: 100
