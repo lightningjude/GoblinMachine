3d02faa31ac8759ca0af47452d2830280fdd6a21
judemiller50 <judemiller50@gmail.com>
2026-02-04 23:44:24 +0000

    Feb 4, 2026, 5:44 PM

--- src/autons.cpp
+++ src/autons.cpp
@@ -206,7 +206,7 @@ auto relturnpoint=[&](float x, float y, int timeout,bool wait=false, lemlib::Tur
     //robot->moveToPose(0, -42.165, 0, 3000,{.forwards=false});
     //chainmovepose(0, -42.165, 0, 10000,false,{.forwards=false});
     robot->moveToPoint(0, -42.165, 10000,{.forwards=false,.minSpeed=30});
-    //waittildone();
+    waittildone();
     robot->turnToHeading(90, 3000,{.direction=AngularDirection::CW_CLOCKWISE});
     matchloader.extend();
     waittildone();
