2f5a149324cfc8a8f06efae8b5875e0c7413c259
judemiller50 <judemiller50@gmail.com>
2026-02-04 02:53:33 +0000

    Feb 3, 2026, 8:53 PM

--- src/autons.cpp
+++ src/autons.cpp
@@ -265,7 +265,7 @@ auto relturnpoint=[&](float x, float y, int timeout,bool wait=false, lemlib::Tur
 
     //ex goal is 30,8, but will hit that way, so need to chain to 10,10
     //so relmove to 10,10 then chainmove to 30,8
-    relmovepose(10, 10, 270, 5000,false,{.forwards=false});
+    relmovepose(10, 10, 270, 5000,false,{.forwards=false,.minSpeed=20});
 
     //robot->moveToPose(0, -35, 180, 3000);
     //robot->turnToHeading(270, 2000,{.direction=AngularDirection::CW_CLOCKWISE});
