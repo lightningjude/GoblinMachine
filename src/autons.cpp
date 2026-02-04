cacfbe0e12139ae66c6444827de1e85b675fab25
judemiller50 <judemiller50@gmail.com>
2026-02-04 03:27:13 +0000

    Feb 3, 2026, 9:27 PM

--- src/autons.cpp
+++ src/autons.cpp
@@ -266,14 +266,14 @@ auto relturnpoint=[&](float x, float y, int timeout,bool wait=false, lemlib::Tur
     //use chainpose to avoid reseting position, so use a relmove then a chainmove
 
     //turning ahead of time
-    relturnpoint(10, 10, 5000,true,{.minSpeed=10,.earlyExitRange=10});
+    relturnpoint(5, 10, 5000,true,{.minSpeed=10,.earlyExitRange=10});
     //ex goal is 30,8, but will hit that way, so need to chain to 10,10
     //so relmove to 10,10 then chainmove to 30,8
     
-    relmovepose(10, 10, 270, 5000,true,{.forwards=false,.minSpeed=30});
+    chainmovepose(10, 10, 270, 5000,true,{.forwards=false,.minSpeed=30});
     chainmovepose(30, 8, 270, 5000,true,{.forwards=false});
 
-    
+
     //robot->moveToPose(0, -35, 180, 3000);
     //robot->turnToHeading(270, 2000,{.direction=AngularDirection::CW_CLOCKWISE});
     /*
