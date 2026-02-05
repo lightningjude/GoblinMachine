a13d3790518e61fab5e5b021dd41ed3486196c2f
judemiller50 <judemiller50@gmail.com>
2026-02-05 00:10:30 +0000

    Feb 4, 2026, 6:10 PM

--- include/main.h
+++ include/main.h
@@ -86,6 +86,7 @@ void autonskillshand(lemlib::Chassis*);
 void pidtest(lemlib::Drivetrain drivetrain,lemlib::OdomSensors sensors,lemlib::ExpoDriveCurve throttle_curve,lemlib::ExpoDriveCurve steer_curve, std::string type);
 void prtdrive(void*);
 void prtheading(void*);
+void piddrive();
 std::tuple<pros::Color,int,int,int,lv_obj_t*> selectgui(void);
 
 #ifdef __cplusplus
