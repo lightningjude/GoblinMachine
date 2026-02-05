0f97248371f443106c067b3bc541e17b9b1ee0de
judemiller50 <judemiller50@gmail.com>
2026-02-05 00:07:07 +0000

    Feb 4, 2026, 6:07 PM

--- src/autons.cpp
+++ src/autons.cpp
@@ -198,7 +198,7 @@ auto relturnpoint=[&](float x, float y, int timeout,bool wait=false, lemlib::Tur
     //start outside end parking, center axel centered on center nut of parking
     robot->setPose(0,0,0);
     pros::delay(20);
-
+    robot->turnToHeading(5, 500);
     //robot->moveToPose(0, -42.165,0,3000,{.forwards=false});
     //waittildone();
     relmovepose(0, -42.165, 0, 3000,true,{.forwards=false});
