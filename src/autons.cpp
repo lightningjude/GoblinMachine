3e72d6e55fe06fafa5cfc4c3b65ef6d604504071
judemiller50 <judemiller50@gmail.com>
2026-02-07 18:50:52 +0000

    Feb 7, 2026, 12:50 PM

--- src/autons.cpp
+++ src/autons.cpp
@@ -288,8 +288,8 @@ auto posreset=[&]() {
     piddrive(10, 2000); 
     for(int i=0;i<2;i++) {
     done=piddrive(4, 800);
-    pros::delay(120);
-    done=piddrive(2,800);
+    pros::delay(130);
+    done=piddrive(-2,800);
     pros::delay(15);
     }
     //report done
