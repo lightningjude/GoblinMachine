e7e49a3251c76a43c1376b57b2f7f4555969086e
judemiller50 <judemiller50@gmail.com>
2026-02-07 16:21:02 +0000

    Feb 7, 2026, 10:21 AM

--- src/autons.cpp
+++ src/autons.cpp
@@ -236,7 +236,7 @@ auto posreset=[&]() {
     //drive across square
     piddrive(24*sqrt(2)-0.5, 2000);
     //turn to face 270, MAY NEED TO CHANGE
-    robot->turnToHeading(270, 1500,{.direction=AngularDirection::CW_CLOCKWISE});
+    robot->turnToHeading(267, 1500,{.direction=AngularDirection::CW_CLOCKWISE});
     waittildone();
     //drive across to other side
     piddrive(53.0/2.0,2000);
